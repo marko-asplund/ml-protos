7e288d25a4e2f68b835f6b6a37c9e82c6361d367
marko asplund <marko.asplund@gmail.com>
2015-01-01 22:57:33 +0200

    migrate to using mutable data structures to avoid outofmemory error; refactor

--- src/main/scala/fi/markoa/proto/ml/itemsets/APriori.scala
+++ src/main/scala/fi/markoa/proto/ml/itemsets/APriori.scala
@@ -3,6 +3,7 @@ package fi.markoa.proto.ml.itemsets
 import scala.annotation.tailrec
 import scala.io.Source
 import scala.collection.{Set, Iterator}
+import scala.collection.{mutable => M}
 
 /*
 implementation of the A-Priori algorithm.
@@ -10,50 +11,85 @@ implementation of the A-Priori algorithm.
  */
 class APriori(inputFileName: String, support: Int, skip: Int, recordSeparator: String) {
 
-  class FrequentItems(val counts: IndexedSeq[Int], val itemMapping: Map[String, Int]) {
-    def +(item: String, count: Int = 1) = if(itemMapping.contains(item))
-      new FrequentItems(counts.updated(itemMapping(item), counts(itemMapping(item)) + count), itemMapping)
-    else
-      new FrequentItems(counts :+ count, itemMapping.updated(item, counts.size))
-    def count(item: String) = counts(itemMapping(item))
+  class FrequentItems(val counts: Vector[Int], val itemMapping: Map[String, Int]) {
     def size() = itemMapping.size
     def contains(item: String) = itemMapping.contains(item)
     def index(item: String) = itemMapping(item)
     def itemCounts = itemMapping.toStream.map( p => (p._1, counts(p._2)) )
-    def countFrequentPairs = APriori.this.countFrequentPairs(pairs = FrequentPairs(this))
+    def countFrequentPairs = APriori.this.countFrequentPairs(items = this)
   }
 
-  object FrequentItems {
-    val Initial = new FrequentItems(Vector(-1), Map())
-  }
-
-  class FrequentPairs(val frequentItems: FrequentItems, val counts: IndexedSeq[Int]) {
-    def +(p: (Int, Int)) = {
-      val k = getPairsIndex(p._1, p._2, frequentItems.size)
-      new FrequentPairs(frequentItems, counts.updated(k, counts(k) + 1))
+  def countFrequentItems(lines: Iterator[String] = getLines()): FrequentItems = {
+    class ItemCounts(val counts: M.ArrayBuffer[Int] = M.ArrayBuffer() :+ -1, val itemMapping: M.Map[String, Int] = M.Map()) {
+      def +(item: String, count: Int = 1) = {
+        if (itemMapping.contains(item)) {
+          counts.update(itemMapping(item), counts(itemMapping(item)) + count)
+        } else {
+          itemMapping.update(item, counts.size)
+          counts.append(count)
+        }
+        this
+      }
     }
-    def getPairsIndex(i: Int, j: Int, n: Int = frequentItems.size) = ((i-1) * (n-i/2.0)).toInt + j - i
 
-    def count(ij: (Int, Int)) = counts(getPairsIndex(ij._1, ij._2))
-    def isFrequent(ij: (Int, Int)) = count(ij._1, ij._2) >= support
+    @tailrec def doCountFrequentItems(items: ItemCounts): FrequentItems = {
+      if (!lines.hasNext) {
+        val r = items.itemMapping.foldLeft(new ItemCounts()) {
+          case (itemsAcc, (item, idx)) if (items.counts(idx) >= support) => itemsAcc +(item, items.counts(idx))
+          case (itemsAcc, _) => itemsAcc
+        }
+        new FrequentItems(r.counts.toVector, r.itemMapping.toMap)
+      } else
+        doCountFrequentItems(lines.next().split(recordSeparator).foldLeft(items)((itemsAcc, item) => itemsAcc + item))
+    }
+    doCountFrequentItems(new ItemCounts())
+  }
 
-    def countFrequentTriples = APriori.this.countFrequentTriples(frequentTriples = FrequentTriples(this))
+  class FrequentPairs(val frequentItems: FrequentItems, val counts: Array[Int]) {
+    def count(p: IndexedSeq[Int]) = counts(FrequentPairs.getPairsIndex(p, frequentItems.size))
+    def isFrequent(p: IndexedSeq[Int]) = count(p) >= support
+
+    def countFrequentTriples = APriori.this.countFrequentTriples(pairs = this)
 
     def pairCounts = {
       val idxToName = for ((k, v) <- frequentItems.itemMapping) yield (v, k)
       for { i <- Stream.range(1, frequentItems.size)
             j <- Stream.range(2, frequentItems.size+1) if (i < j)
-            k = getPairsIndex(i, j) if (counts(k) >= support)
+            k = FrequentPairs.getPairsIndex(IndexedSeq(i, j), frequentItems.size) if (counts(k) >= support)
             t = (idxToName(i), idxToName(j))
-      } yield (if(t._1 < t._2) t else (t._2, t._1)) -> counts(k)
+      } yield (if(t._1 < t._2) t else t.swap) -> counts(k)
     }
   }
 
   object FrequentPairs {
     def apply(items: FrequentItems) =
       new FrequentPairs(items, new Array[Int](getTriangularMatrixSize(items.itemMapping.size)))
+    def getPairsIndex(p: IndexedSeq[Int], n: Int) = ((p(0)-1) * (n-p(0)/2.0)).toInt + p(1) - p(0)
     def getTriangularMatrixSize(n: Int) = ((n-1) * n/2.0).toInt + 1
   }
+
+  private def generateCandidatePairsInBasket(basket: String, items: FrequentItems) = {
+    basket.split(recordSeparator).collect {
+      case x if(items.contains(x)) => items.index(x)
+    }.toSet.subsets(2).map( s => s.toIndexedSeq.sorted )
+  }
+
+  def countFrequentPairs(lines: Iterator[String] = getLines(), items: FrequentItems) = {
+    @tailrec def doCountFrequentPairs(counts: Array[Int]): FrequentPairs = {
+      if (!lines.hasNext) {
+        new FrequentPairs(items, counts)
+      } else {
+        // iterate items in basket; filter out non-frequent items; generate pairs, order items in pair
+        doCountFrequentPairs(generateCandidatePairsInBasket(lines.next(), items).foldLeft(counts) { (countsAcc, p) =>
+          val k = FrequentPairs.getPairsIndex(p, items.size)
+          countsAcc.update(k, countsAcc(k) + 1)
+          countsAcc
+        })
+      }
+    }
+    doCountFrequentPairs(new Array[Int](FrequentPairs.getTriangularMatrixSize(items.size)))
+  }
+
 
   class FrequentTriples(val frequentPairs: FrequentPairs, val counts: Map[List[Int], Int]) {
     val idxToName = for ((k, v) <- frequentPairs.frequentItems.itemMapping) yield (v, k)
@@ -65,64 +101,42 @@ class APriori(inputFileName: String, support: Int, skip: Int, recordSeparator: S
       new FrequentTriples(pairs, Map())
   }
 
+  // can be generalized from triplets to k-sets
+  def countFrequentTriples(lines: Iterator[String] = getLines(), pairs: FrequentPairs): FrequentTriples = {
+    @tailrec def doCountFrequentTriples(counts: M.Map[List[Int], Int]): FrequentTriples = {
+      if(!lines.hasNext) {
+        new FrequentTriples(pairs, counts.toMap)
+      } else {
+        val c1 = generateCandidatePairsInBasket(lines.next(), pairs.frequentItems).
+          filter( p => pairs.isFrequent(p) ).toList
+        val itemCounts = c1.flatMap(p => p.toList).groupBy(v => v).mapValues(_.size)
+        val c2 = c1.filter( p => p.forall(itemCounts(_) >= 2) )
+        val c3 = (for(i <- c2.zipWithIndex;
+                      j <- c2.zipWithIndex if(i._2 != j._2);
+                      s = i._1.toSet ++ j._1 if(s.size == 3);
+                      if(s.subsets(2).forall( p => pairs.isFrequent(p.toIndexedSeq.sorted)) )
+        ) yield(s.toList.sorted)).toSet
+        doCountFrequentTriples(c3.foldLeft(counts)( (countsAcc, i) => {
+          countsAcc.update(i, countsAcc.getOrElse(i, 0) + 1)
+          countsAcc
+        }))
+      }
+    }
+    doCountFrequentTriples(M.Map())
+  }
+
   def getLines() = Source.fromFile(inputFileName).getLines().drop(skip)
 
-  @tailrec final def countFrequentItems(lines: Iterator[String] = getLines(), items: FrequentItems = FrequentItems.Initial): FrequentItems = {
-    if(!lines.hasNext)
-      items.itemMapping.foldLeft(FrequentItems.Initial) {
-        case (itemsAcc, (item, idx)) if (items.counts(idx) >= support) => itemsAcc +(item, items.counts(idx))
-        case (itemsAcc, _) => itemsAcc
-      }
-    else
-      countFrequentItems(lines, lines.next().split(recordSeparator).foldLeft(items)( (itemsAcc, item) => itemsAcc + item ))
-  }
-
-  def generateCandidatePairsInBasket(basket: String, pairs: FrequentPairs) = {
-    basket.split(recordSeparator).collect {
-      case x if(pairs.frequentItems.contains(x)) => pairs.frequentItems.index(x) }.
-      toSet.subsets(2).map( s => s.toIndexedSeq match {
-      case Seq(a, b) => if (a < b) (a, b) else (b, a)
-    })
-  }
-
-  @tailrec final def countFrequentPairs(lines: Iterator[String] = getLines(), pairs: FrequentPairs): FrequentPairs = {
-    if (!lines.hasNext) {
-      pairs
-    } else {
-      // iterate items in basket; filter out non-frequent items; generate pairs, order items in pair
-      countFrequentPairs(lines, generateCandidatePairsInBasket(lines.next(), pairs).foldLeft(pairs) { (countsAcc, p) =>
-        countsAcc + p
-      })
-    }
-  }
-
-  // can be generalized from triplets to k-sets
-  @tailrec final def countFrequentTriples(lines: Iterator[String] = getLines(), frequentTriples: FrequentTriples): FrequentTriples = {
-    import frequentTriples.frequentPairs
-    if(!lines.hasNext) {
-      frequentTriples
-    } else {
-      val c1 = generateCandidatePairsInBasket(lines.next(), frequentPairs).
-        filter( p => frequentPairs.isFrequent(p) ).toList
-      val itemCounts = c1.flatMap(p => List(p._1, p._2)).groupBy(v => v).mapValues(_.size)
-      val c2 = c1.filter( p => itemCounts(p._1) >= 2 && itemCounts(p._2) >= 2)
-      val c3 = (for(i <- c2.zipWithIndex;
-          j <- c2.zipWithIndex if(i._2 != j._2);
-          s = Set(i._1._1, i._1._1, j._1._1, j._1._2) if(s.size == 3);
-          if(s.subsets(2).forall( sub => sub.toIndexedSeq match {
-            case Seq(a, b) => frequentPairs.isFrequent(if(a < b) (a, b) else (b, a))
-          }))
-      ) yield(s.toList.sorted)).toSet
-      val ft = c3.foldLeft(frequentTriples.counts)( (countsAcc, i) =>
-        countsAcc.updated(i, countsAcc.getOrElse(i, 0) + 1)
-      )
-      countFrequentTriples(lines, new FrequentTriples(frequentTriples.frequentPairs, ft))
-    }
-  }
 }
 
 object APriori {
   def apply(inputFileName: String, support: Int) = {
     new APriori(inputFileName, support, 1, " ")
+  }
+
+  def main(args: Array[String]) = {
+    val pairs = APriori(args(0), 2).countFrequentItems().countFrequentPairs
+    for(p <- pairs.pairCounts)
+      println(s"${p._1}: ${p._2}")
   }
 }
